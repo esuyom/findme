55daf9423afff76af3d5b7fbc40d1e04ed1f314b
esuyom <sylee0560@gmail.com>
2026-05-11 16:19:39 +0900

    파일수정

--- src/pages/mypage/CpRecruitListPage.jsx
+++ src/pages/mypage/CpRecruitListPage.jsx
@@ -46,10 +46,10 @@ export default function CpRecruitListPage() {
                 key={tab.key}
                 className={`part col${activeTab === tab.key ? ' active' : ''}`}
               >
-                <button type="button" onClick={() => setActiveTab(tab.key)}>
+                <Link onClick={() => setActiveTab(tab.key)}>
                   <div className="text">{tab.label}</div>
                   <div className={`num${tab.count === 0 ? ' zero' : ''}`}>{tab.count}</div>
-                </button>
+                </Link>
               </div>
             ))}
           </div>
